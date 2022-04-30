7b59a29f83a7876b882098168261ee4cb40f3eb7
rvorster <royvorster@gmail.com>
2022-04-30 16:58:53 +0200

    update simple example

--- example.py
+++ example.py
@@ -1,12 +1,32 @@
-from alsograd.core import *
-from alsograd.operations import *
+import alsograd.core as ag
+from alsograd.nn.module import Module
+from alsograd.nn.layers import Linear
+from alsograd.nn.loss import MSE
+from alsograd.nn.activations import ReLU
 
-x = Parameter.ones(10, 1)
-yy = Parameter(5)
-y = Parameter.ones(10, 1)*yy
 
-z = (x + y).sum()
-z.backward()
+class Net(Module):
+    def __init__(self):
+        super().__init__()
 
-print(z, z.grad)
-print(yy.grad, y.grad, x.grad)
+        # Layers
+        self.l1 = Linear(10, 128)
+        self.l2 = Linear(128, 128)
+        self.l3 = Linear(128, 2)
+
+        # Non-linearity
+        self.act = ReLU()
+
+    def forward(self, x):
+        x = self.act(self.l1(x))
+        x = self.act(self.l2(x))
+        return self.l3(x)
+
+
+# Simple forward pass
+model = Net()
+x = ag.Parameter.init(1, 10)
+y = model(x)
+
+y.backward()
+print(model.l1.w.grad)
