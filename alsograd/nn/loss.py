78ba4c2fdd00e5bab2037cde8fd85280a29854c4
rvorster <royvorster@gmail.com>
2022-05-01 15:52:50 +0200

    fix mse

--- alsograd/nn/loss.py
+++ alsograd/nn/loss.py
@@ -2,4 +2,4 @@ from alsograd.core import Parameter
 
 
 def MSE(y_true: Parameter, y_pred: Parameter) -> Parameter:
-    return ((y_true - y_pred)**2).sum()
+    return ((y_true - y_pred)**2).mean()
