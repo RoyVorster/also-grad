e285ed2accac8fb0d5a1f8e6901ecb981345d993
rvorster <royvorster@gmail.com>
2022-05-01 20:56:49 +0200

    add softmax and logsumexp

--- alsograd/nn/activations.py
+++ alsograd/nn/activations.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from alsograd.core import Operation
+from alsograd.core import Parameter, Operation
 
 
 class ReLU(Operation):
@@ -11,3 +11,23 @@ class ReLU(Operation):
     def backward(self, g: np.ndarray) -> np.ndarray:
         a, = self.cache
         return g*(a >= 0)
+
+
+def relu(a: Parameter) -> Parameter:
+    return ReLU()(a)
+
+
+def softmax(a: Parameter) -> Parameter:
+    new_shape = list(a.shape)
+    new_shape[-1] = 1
+
+    err = (a - a.max(axis=-1).reshape(*new_shape)).exp()
+    return err/err.sum(axis=-1).reshape(*new_shape)
+
+
+def log_softmax(a: Parameter) -> Parameter:
+    new_shape = list(a.shape)
+    new_shape[-1] = 1
+
+    a_max = a.max(axis=-1).reshape(*new_shape)
+    return a_max + (a - a_max).exp().sum(axis=-1).reshape(*new_shape).log()
