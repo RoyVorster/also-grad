59fe0d2f27cdb722b95f509c9182858a23d3719c
rvorster <royvorster@gmail.com>
2022-05-01 21:02:56 +0200

    cleanup SGD optimizer - also update readme

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Wanted to build one myself
-Just autograd. Really simple. Plan is to write one in Rust and use macros for compile-time backprop.
-
+Just autograd. Really simple.
--- alsograd/nn/optimizer.py
+++ alsograd/nn/optimizer.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Callable, List
 
-from alsograd.core import Parameter, no_grad
+from alsograd.core import Parameter
 from alsograd.nn.module import Module
 
 
@@ -17,17 +17,15 @@ class Optimizer:
         self.model.zero_grad()
 
     def step(self):
-        with no_grad():
-            for p in self.parameters:
-                self.parameter_step(p)
+        for p in self.parameters:
+            self.parameter_step(p)
 
 
 class SGD(Optimizer):
     def __init__(self, model: Module, learning_rate: float) -> None:
         super().__init__(model)
-
         self.learning_rate = learning_rate
 
     def parameter_step(self, parameter: Parameter) -> None:
         if parameter.grad:
-            parameter -= Parameter([self.learning_rate], requires_grad=False)*parameter.grad
+            parameter -= self.learning_rate*parameter.grad
