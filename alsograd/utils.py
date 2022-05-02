fix reverse broadcasting

alsograd/utils.py
@@ -2,10 +2,10 @@ from typing import Tuple, Union, Iterable, Any
 import numpy as np
 
 
-# Backwards pass through sum operation
+# Backwards pass through np broadcasting
 def rev_sum(x: np.ndarray, shape: Tuple[int]):
     axis = [0] if shape == (1, ) else [i for i, s in enumerate(shape) if s == 1 and x.shape[i] > 1]
-    return x.sum(axis=axis).reshape(shape) if len(axis) > 0 else x
+    return x.sum(axis=tuple(axis)).reshape(shape) if len(axis) > 0 else x
 
 
 def plural(x: Union[Iterable[Any], Any]) -> Tuple[Any, ...]: