2e0176cf8228e9ede7fb581e3b4c79e6fd3def0f
rvorster <royvorster@gmail.com>
2022-05-03 16:22:05 +0200

    fix bias in linear layer

--- alsograd/nn/layers.py
+++ alsograd/nn/layers.py
@@ -5,8 +5,10 @@ from alsograd.nn.module import Module
 class Linear(Module):
     def __init__(self, in_neurons: int, out_neurons: int):
         super().__init__()
+
         self.w = Parameter.init(in_neurons, out_neurons)
-        self.b = Parameter.init(out_neurons)
+        self.b = Parameter.zeros(out_neurons)
 
     def forward(self, x: Parameter) -> Parameter:
-        return x@self.w + self.b
+        b_shape = [1]*(x.ndim - 1) + [-1]  # Batch reshape
+        return x@self.w + self.b.reshape(*b_shape)
