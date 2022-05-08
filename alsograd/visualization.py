785623248daaf610be4346229f3f04bcb2479caf
rvorster <royvorster@gmail.com>
2022-05-08 16:32:19 +0200

    add colors for require grad in graphviz. add edge label option

--- alsograd/visualization.py
+++ alsograd/visualization.py
@@ -3,18 +3,26 @@ from graphviz import Digraph
 from alsograd import Parameter
 
 
-def create_graph(p: Parameter, render=False, f_name="compute_graph", backward_order=True) -> Digraph:
+def create_graph(p: Parameter, render=False, f_name="compute_graph", backward_order=True,
+                 show_edge_labels=False) -> Digraph:
     graph = Digraph("g", strict=False)
+
+    color = lambda n: 'green' if n.requires_grad else 'red'
 
     nodes = p.topography()
     for node in nodes:
         if not node.builder:
             continue
 
-        graph.node(node.label)
+        graph.node(node.label, style='filled', fillcolor=color(node))
         for parent in filter(lambda p: p is not None, node.builder.parents):
+            if not parent in graph:
+                graph.node(parent.label, style='filled', fillcolor=color(parent))
+
             s = (node.label, parent.label) if backward_order else (parent.label, node.label)
-            graph.edge(*s)
+
+            edge_label = node.builder.__class__.__name__ if show_edge_labels else ""
+            graph.edge(*s, label=edge_label)
 
     if render:
         graph.render(f_name, view=True)
--- examples/simple_function.py
+++ examples/simple_function.py
@@ -1,11 +1,16 @@
 import numpy as np
+
 import alsograd.core as ag
+import alsograd.visualization as viz
 
 x = ag.Parameter(np.linspace(1, 20, 1000))
 y = (x.sin() + 1).exp()/x.sqrt()
 
 y.backward()  # Populate gradients
 assert x.grad is not None
+
+x.label, y.label = "x", "y"
+viz.create_graph(y, backward_order=False, render=True)
 
 # Plot
 import matplotlib.pyplot as plt
